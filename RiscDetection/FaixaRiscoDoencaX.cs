eeded0df822cd361937e4727b1676dd2f9907390
Dyego Maas <dyego.maas@gmail.com>
2020-04-27 14:18:24 -0300

    Aplicação de exemplo.

---
+++ RiscDetection/FaixaRiscoDoencaX.cs
@@ -0,0 +1,10 @@
+namespace RiscDetection
+{
+    public class FaixaRiscoDoencaX
+    {
+        public bool FaixaRisco(int idade)
+        {
+            return idade > 18 && idade <= 35;
+        }
+    }
+}
---
+++ RiscDetectionTest/FaixaRiscoDoencaXTest.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using RiscDetection;
+
+namespace RiscDetectionTest
+{
+    public class FaixaRiscoDoencaXTest
+    {
+        [Test]
+        public void pessoa_com_20_anos_esta_na_zona_de_risco()
+        {
+            new FaixaRiscoDoencaX().FaixaRisco(20).Should().BeTrue();
+        }
+        
+        [Test]
+        public void pessoa_com_15_anos_esta_fora_da_zona_de_risco()
+        {
+            new FaixaRiscoDoencaX().FaixaRisco(15).Should().BeFalse();
+        }
+    }
+}
